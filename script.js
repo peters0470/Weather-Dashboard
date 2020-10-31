2893ab6edf811ec75be90c8ccaf46d71521a329b
unknown <peters0470@gmail.com>
2020-10-30 19:11:58 -0500

    added id variables to javascript and started to give value to html elements

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
 var APIKey = "57ef72e4f3efee9ee21528ce834c61ef";
+var cityEl = document.getElementById("city-input");
+var searchCityEl = document.getElementById("search-button");
+var clearHistoryEl = document.getElementById("clear-history");
+var cityNameEl = document.getElementById("city-name");
+var weatherPicEl = document.getElementById("current-pic");
+var currentTemperatureEl = document.getElementById("temperature");
+var currentHumidityEl = document.getElementById("humidity");
+var currentWindSpeedEl = document.getElementById("wind-speed");
+var currentUvEl = document.getElementById("UV-index");
+var historyEl = document.getElementById("history");
 
 function getWeather(cityName) {
     var weatherURL = "https://api.openweathermap.org/data/2.5.weather?q=" + cityName + "&appid=" + APIKey;
@@ -13,7 +23,13 @@ function getWeather(cityName) {
         var year = currentDate.getFullYear();
         var weatherPic = data.weather[0].icon;
         var lon = data.coord.lon;
-        var lat = data.coord.lon;
+        var lat = data.coord.lat;
+
+        weatherPicEl.setAttribute("src","https://openweathermap.org/img/wn/" + weatherPic + "@2x.png");
+        weatherPicEl.setAttribute("alt",data.weather[0].description);
+        cityNameEl.innerHTML = data.name + " (" + month + "/" + day + "/" + year + ") ";
+        currentHumidityEl.innerHTML = "Humidity: " + data.main.humidity + "%";
+        currentWindSpeedEl.innerHTML = "Wind Speed: " + data.wind.speed + "mph";
 
 
      })
